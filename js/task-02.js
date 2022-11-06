64815133342e73ed01a1b69421e0fc8b08e9264b
Denys Konopatkin <k.denis.aa@gmail.com>
2022-11-06 14:38:00 +0200

    task-02-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refs = {
+  ul: document.querySelector('#ingredients'),
+};
+
+const list = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredient;
+  return li;
+});
+
+refs.ul.append(...list);
+
+// -----------------------------------------------
+// refs.ingredientsEl.append(li);
+
+// const getItemTemplate = (text) => `<li class="item">${text}</li>`;
+
+// const lis = ingredients.map((ingredient) => getItemTemplate(ingredient));
+// refs.ingredientsEl.insertAdjacentHTML('beforeend', lis.join(''));
