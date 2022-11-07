d7336045b74466701a542434d5f7c570320195a1
Denys Konopatkin <k.denis.aa@gmail.com>
2022-11-07 23:36:33 +0200

    task-09-done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+};
+
+refs.button.addEventListener('click', (e) => {
+  const randomHex = getRandomHexColor();
+  document.body.style.backgroundColor = randomHex;
+  refs.span.textContent = randomHex;
+});
